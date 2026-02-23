11dc79b6650aecd3124f52370befddb7ca617844
teknolojiparibu-oss <teknolojiparibu@gmail.com>
2026-02-23 21:42:09 +0300

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php session_set_cookie_params(['lifetime' => 0, 'path' => '/', 'secure' => true, 'httponly' => true, 'samesite' => 'None']); session_start(); ?>
 <!DOCTYPE html>
 <html lang="tr">
 <head>
